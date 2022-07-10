2163a53514f42cef2f908369d73f750f4dba3d3c
akanchha.ece@gmail.com <akanchha.ece@gmail.com>
2022-07-10 11:00:12 +0200

    chapter 2 learning

---
+++ course 2-API Development and Documentation/lesson2/FlaskApp/flaskr/app.py
@@ -0,0 +1,13 @@
+# Import your dependencies
+from flask import Flask, jsonify
+# Define the create_app function
+def create_app(test_config=None):
+ # Create and configure the app
+ # Include the first parameter: Here, __name__is the name of the current Python module.
+ app = Flask(__name__)
+
+ @app.route('/smiley')
+ def smiley():
+     return ':)'
+ # Return the app instance
+ return app
